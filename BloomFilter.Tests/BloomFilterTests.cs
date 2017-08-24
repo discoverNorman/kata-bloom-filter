216c50aeead33b61a147cf206489302dc0ae3db7
b <b@LAPTOP-N3QDMOLD>
2017-08-24 12:16:01 -0400

    Moved the hash tests to it's own file.
    Signed-off-by: b <b@LAPTOP-N3QDMOLD>

--- BloomFilter.Tests/BloomFilterTests.cs
+++ BloomFilter.Tests/BloomFilterTests.cs
@@ -45,37 +45,6 @@ namespace BloomFilter.Tests
 
             Assert.IsTrue(result1, $"{testWord2} was not added to the filter, and should be the only item in the filter.  Your filter/ hash is broken");
         }
-
-        /// <summary>
-        /// Test to check that the Simple Hash hashes the same for each object.
-        /// </summary>
-        [TestMethod]
-        public void HashTestPositive()
-        {
-            var hash = new SimpleHash();
-
-            var testWord = "blarg";
-            var testWord2 = "blarg";
-            var result1 = hash.Hash(testWord);
-            var result2 = hash.Hash(testWord2);
-
-            Assert.AreEqual(result1, result2, $"{result1} and {result2} should be identical, the hash algorithm has errors.");
-        }
-
-        /// <summary>
-        /// Negative test to check that the Simple Hash hashes the same for each object.
-        /// </summary>
-        [TestMethod]
-        public void HashTestMegative()
-        {
-            var hash = new SimpleHash();
-
-            var testWord = "blarg";
-            var testWord2 = "blargagain";
-            var result1 = hash.Hash(testWord);
-            var result2 = hash.Hash(testWord2);
-
-            Assert.AreNotEqual(result1, result2, $"{result1} and {result2} should not be identical, the hash algorithm has errors.");
-        }
+        
     }
 }
---
+++ BloomFilter.Tests/SimpleHashTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BloomFilter.Tests
+{
+    [TestClass]
+    public class SimpleHashTest
+    {
+
+        /// <summary>
+        /// Test to check that the Simple Hash hashes the same for each object.
+        /// </summary>
+        [TestMethod]
+        public void HashTestPositive()
+        {
+            var hash = new SimpleHash();
+
+            var testWord = "blarg";
+            var testWord2 = "blarg";
+            var result1 = hash.Hash(testWord);
+            var result2 = hash.Hash(testWord2);
+
+            Assert.AreEqual(result1, result2, $"{result1} and {result2} should be identical, the hash algorithm has errors.");
+        }
+
+        /// <summary>
+        /// Negative test to check that the Simple Hash hashes the same for each object.
+        /// </summary>
+        [TestMethod]
+        public void HashTestMegative()
+        {
+            var hash = new SimpleHash();
+
+            var testWord = "blarg";
+            var testWord2 = "blargagain";
+            var result1 = hash.Hash(testWord);
+            var result2 = hash.Hash(testWord2);
+
+            Assert.AreNotEqual(result1, result2, $"{result1} and {result2} should not be identical, the hash algorithm has errors.");
+        }
+    }
+}
