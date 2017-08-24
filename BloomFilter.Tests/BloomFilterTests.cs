73a51a2bc8bb39331a5afc5aaa08ed2e3fb28c3f
b <b@LAPTOP-N3QDMOLD>
2017-08-24 10:32:42 -0400

    Initial Tests to set to failure to assist in driving the approriate design pattern. All tests should fail or be inconclusive
    Signed-off-by: b <b@LAPTOP-N3QDMOLD>

---
+++ BloomFilter.Tests/BloomFilterTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BloomFilter.Tests
+{
+    [TestClass]
+    public class BloomFilterTests
+    {
+        /// <summary>
+        /// A Bloom filter is a method for determining whether an item is a member of a set. It's
+        ///useful for checking membership in a very large set without using excessive memory.
+        /// </summary>
+        [TestMethod]
+        public void MainLineTest()
+        {
+            Assert.Inconclusive("This is the main test that should pass to indicate that the bloom filter works.");
+        }
+
+        [TestMethod]
+        public void HashTest()
+        {
+            Assert.Inconclusive("This is the first of many hash tests to ensure hashes are unique.  This might be replaced by a tested library");
+        }
+    }
+}
